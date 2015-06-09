6866f6f505754e9d0c2b3874b0a0ebbe64e6762b
Pascal <pascal@pascal.com>
2015-06-09 00:29:16 -0400

    Initial Commit

---
+++ Free Ruler/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Free Ruler
+//
+//  Created by Pascal on 6/9/15.
+//  Copyright (c) 2015 Pascal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
