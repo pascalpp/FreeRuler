9329e8a777ed44608b2fa1b84df8d6b4a185e1be
Pascal <pascal@pascal.com>
2026-06-13 20:33:23 -0400

    Use timestamped help icon filenames
    Switch help icon naming from a build-number-based token to a UTC timestamp+hash token. Update FreeRuler HTML and CSS to reference the new freeruler-help-icon-<timestamp>-<hash>.png name, rename the icon file accordingly, and remove the old freeruler.png legacy copy. Update scripts/generate-app-icon.sh to generate a timestamped help_cache_token, drop the Info.plist build_number dependency, remove legacy copy behavior, and propagate the new token into styles.css and FreeRuler.html while cleaning up stale icons.

--- scripts/generate-app-icon.sh
+++ scripts/generate-app-icon.sh
@@ -8,8 +8,6 @@ help_resource_dir="$PWD/Free Ruler/FreeRuler.help/Contents/Resources"
 help_shared_dir="$help_resource_dir/shrd"
 help_html="$help_resource_dir/English.lproj/FreeRuler.html"
 help_css="$help_shared_dir/styles.css"
-help_info="$PWD/Free Ruler/FreeRuler.help/Contents/Info.plist"
-legacy_help_icon="$help_shared_dir/freeruler.png"
 binary="${TMPDIR:-/tmp}/freeruler-generate-app-icon"
 module_cache="${TMPDIR:-/tmp}/freeruler-generate-app-icon-module-cache"
 
@@ -24,13 +22,13 @@ xcrun swiftc \
 "$binary" "$output_dir"
 
 icon_hash="$(shasum -a 256 "$output_dir/icon_512x512.png" | awk '{ print substr($1, 1, 12) }')"
-build_number="$(/usr/libexec/PlistBuddy -c 'Print :CFBundleVersion' "$help_info")"
-build_token="$(printf '%s' "$build_number" | tr -cd '[:alnum:]_.-')"
-help_icon_name="freeruler-help-icon-build-${build_token}-${icon_hash}.png"
+generated_at="$(date -u +%Y%m%d%H%M%S)"
+help_cache_token="${generated_at}-${icon_hash}"
+help_icon_name="freeruler-help-icon-${help_cache_token}.png"
 help_icon="$help_shared_dir/$help_icon_name"
 
 cp "$output_dir/icon_512x512.png" "$help_icon"
-cp "$output_dir/icon_512x512.png" "$legacy_help_icon"
+rm -f "$help_shared_dir/freeruler.png"
 
 for stale_icon in "$help_shared_dir"/freeruler-help-icon-*.png; do
   if [[ -e "$stale_icon" && "$stale_icon" != "$help_icon" ]]; then
@@ -42,6 +40,6 @@ HELP_ICON_NAME="$help_icon_name" perl -0pi -e \
   's#background-image: url\(\./(?:freeruler\.png|freeruler-help-icon-[^)]+)\);#background-image: url(./$ENV{HELP_ICON_NAME});#' \
   "$help_css"
 
-HELP_CACHE_TOKEN="build-${build_token}-${icon_hash}" perl -0pi -e \
+HELP_CACHE_TOKEN="$help_cache_token" perl -0pi -e \
   's#href="\.\./shrd/styles\.css(?:\?[^"]*)?"#href="../shrd/styles.css?icon=$ENV{HELP_CACHE_TOKEN}"#' \
   "$help_html"
